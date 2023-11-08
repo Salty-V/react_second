1812265addf1cfc78f90ff97c75aa33797def5d7
Camille Boujasson <camilleb.dev@outlook.fr>
2023-11-08 13:54:42 +0100

    added objects in array

--- src/Products.js
+++ src/Products.js
@@ -1,13 +1,25 @@
 function Products() {
 
-    const passAvailable = ["Pass Annuel TOP14 Standard","Pass jeunes passionnés", "Pass tu passes pas"]
+    const passAvailable = 
+   
+    [{  title : "Pass Annuel TOP14 Standard",
+        price : "650€",
+        isPublished : true, },
+
+    {   title : "Pass jeunes passionnés",
+        price : "333€",
+        isPublished : true, },
+
+    {   title : "Pass tu passes pas",
+        price : "2200€",
+        isPublished : true, }]
 
     return (
     <main>
        {passAvailable.map((product) => {
             return (
             <article>
-                <h3>{product}</h3>
+                <h3>{product.title}</h3>
             </article>
             );
        })}
